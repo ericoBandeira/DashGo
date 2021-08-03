d87d915e45964406f47151c6dccad510dc132fda
ericoBandeira <ericobandeira@hotmail.com>
2021-08-03 14:12:06 -0300

    arrumando site

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Text, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header(){
     return(
@@ -45,6 +45,37 @@ export function Header(){
                     _placeholder={{color:'gray.400'}}
                 />
                 <Icon as={RiSearchLine} fontSize="20"/>
+            </Flex>
+
+            <Flex
+                align="center"
+                ml="auto"
+
+            >
+                <HStack 
+                    spacing="4"
+                    mx="8"
+                    pr="8"
+                    py="1"
+                    color="gray.300"
+                    borderRightWidth={1}
+                    borderColor="gray.700"
+                >
+                    <Icon as={RiNotificationLine} fontSize="20"/>
+                    <Icon as={RiUserAddLine} fontSize="20"/>
+                </HStack>
+
+                <Flex
+                    align="center"
+                >
+                    <Box mr="4" textAlign="right">
+                        <Text>Érico Bandeira</Text>
+                        <Text color="gray.300" fontSize="small">
+                            ericobandeira@hotmail.com
+                        </Text>
+                    </Box>
+                    <Avatar size="md" name="Érico Bandeira" src="https://github.com/ericoBandeira.png"/>
+                </Flex>
             </Flex>
         </Flex>
     );
