1cdf4b60ddc4db71fb25f3e727db908eaeaac823
ericoBandeira <ericobandeira@hotmail.com>
2021-08-05 12:45:56 -0300

    adicionando contextos e colocando responsividade

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,16 +1,23 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile(){
+interface ProfileProps{
+    showProfileData?: boolean;
+}
+
+export function Profile({showProfileData}: ProfileProps){
     return(
         <Flex
             align="center"
         >
-            <Box mr="4" textAlign="right">
-                <Text>Érico Bandeira</Text>
-                <Text color="gray.300" fontSize="small">
-                    ericobandeira@hotmail.com
-                </Text>
-            </Box>
+            {showProfileData&&
+                <Box mr="4" textAlign="right">
+                    <Text>Érico Bandeira</Text>
+                    <Text color="gray.300" fontSize="small">
+                        ericobandeira@hotmail.com
+                    </Text>
+                </Box>
+            }
+            
             <Avatar size="md" name="Érico Bandeira" src="https://github.com/ericoBandeira.png"/>
         </Flex>
     );
